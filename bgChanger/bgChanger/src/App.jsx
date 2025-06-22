29526f1ed6b78a64209cd2896916639bdf1c2f65
hariom-cs <hariom_m241042cs@nitc.ac.in>
2025-06-22 16:07:50 +0530

    bg-changer Complete

--- bgChanger/bgChanger/src/App.jsx
+++ bgChanger/bgChanger/src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='w-full h-screen bg-red-500'
+     style={{backgroundColor:color}}
+     >
+      <div className='fixed flex flex-wrap justify-center items-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-4 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button className="text-white px-4 py-2 rounded-3xl hover:bg-red-600 transition"
+          style={{backgroundColor:'red'}}
+          onClick={()=>setColor('red')}>
+            Red
+          </button>
+          <button className=" text-white px-4 py-2 rounded-3xl hover:bg-red-600 transition" 
+          style={{backgroundColor:'blue'}}onClick={()=>setColor('blue')}>
+            blue
+          </button>
+          <button className=" text-white px-4 py-2 rounded-3xl hover:bg-red-600 transition" 
+          style={{backgroundColor:'green'}}onClick={()=>setColor('green')}>
+            green
+          </button>
+          <button className="text-white px-4 py-2 rounded-3xl hover:bg-red-600 transition" 
+          style={{backgroundColor:'grey'}}onClick={()=>setColor('grey')}>
+            grey
+          </button>
+          <button className=" text-white px-4 py-2 rounded-3xl hover:bg-red-600 transition" 
+          style={{backgroundColor:'indigo'}}onClick={()=>setColor('indigo')}>
+            indigo
+          </button>
+          <button className=" text-white px-4 py-2 rounded-3xl hover:bg-red-600 transition" 
+          style={{backgroundColor:'black'}}onClick={()=>setColor('black')}>
+            Black
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
   )
 }
 
